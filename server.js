86ef280af73a9f070fe7be0cbdd0c94fd4ea3a73
shahriarshakil <shahriarshakil500@gmail.com>
2024-03-24 05:20:06 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const port = 3019;
+
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://127.0.0.1:27017/studesData", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("mongodb connection successfully established");
+});
+
+const userSchema = new mongoose.Schema({
+  regd_no: String,
+  name: String,
+  email: String,
+  branch: String,
+});
+
+const Users = mongoose.model("data", userSchema);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "form.html"));
+});
+
+app.post("/post", async (req, res) => {
+  const { regd_no, name, email, branch } = req.body;
+  const user = new Users({
+    regd_no,
+    name,
+    email,
+    branch,
+  });
+  await user.save();
+  console.log(user);
+  res.send("form submission successfully");
+});
+
+app.listen(port, () => {
+  console.log("server started");
+});
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const path = require("path");
+// const port = 3019;
+
+// const app = express();
+// app.use(express.static(__dirname));
+// app.use(express.urlencoded({ extended: true }));
+
+// mongoose.connect("mongodb://127.0.0.1:27017/studesData");
+// const db = mongoose.connection;
+// db.once("open", () => {
+//   console.log("mongodb connection suscessfully established");
+// });
+
+// const userSchema = new mongoose.Schema({
+//   regd_no: String,
+//   name: String,
+//   email: String,
+//   branch: String,
+// });
+
+// const Users = mongoose.model("data", userSchema);
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "form.html"));
+// });
+
+// app.post("/post", async (req, res) => {
+//   const user = new ({Users
+//     regd_no,
+//     name,
+//     email,
+//     branch
+// });
+// await user.save();
+// console.log(user)
+// res.send("form submission successfully")
+// });
+
+// app.listen(port, () => {
+//   console.log("server started");
+// });
